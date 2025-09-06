557b9049816255bcef46806e5f7c2754b0484940
jacobot1 <155650471+jacobot1@users.noreply.github.com>
2025-09-06 17:28:14 -0500

    add files

---
+++ LethalKnockback/Patches/ShotgunItemPatch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace LethalKnockback.Patches
+{
+    [HarmonyPatch(typeof(ShotgunItem))]
+    internal class ShotgunItemPatch
+    {
+        static float currentKnockbackForce = 0f;
+
+        [HarmonyPatch("ShootGun")]
+        [HarmonyPostfix]
+        static void AddKnockbackPatch(ShotgunItem __instance)
+        {
+            // Knock back player
+            if (!__instance.safetyOn && __instance.playerHeldBy == GameNetworkManager.Instance.localPlayerController && __instance.isHeld)
+            {
+                currentKnockbackForce += LethalKnockbackMod.configKnockbackForce.Value;
+            }
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void ContinuousKnockbackPatch(ShotgunItem __instance)
+        {
+            // Continuous knockback
+            __instance.playerHeldBy.externalForces += __instance.transform.forward * -currentKnockbackForce;
+            currentKnockbackForce = Mathf.Lerp(currentKnockbackForce, 0f, 5f * Time.deltaTime);
+        }
+    }
+}
---
+++ LethalKnockback/Plugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalKnockback.Patches;
+
+namespace LethalKnockback
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LethalKnockbackMod : BaseUnityPlugin
+    {
+        public const string modGUID = "com.jacobot5.LethalKnockback";
+        public const string modName = "LethalKnockback";
+        public const string modVersion = "1.0.0";
+
+        // Initalize Harmony
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        // Create static instance
+        private static LethalKnockbackMod Instance;
+
+        // Configuration
+        public static ConfigEntry<float> configKnockbackForce;
+
+        // Initialize logging
+        public static ManualLogSource mls;
+
+        private void Awake()
+        {
+            // Ensure static instance
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            // Send alive message
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("LethalKnockback has awoken.");
+
+            // Bind configuration
+            configKnockbackForce = Config.Bind("Knockback",
+                                                "KnockbackForce",
+                                                15f,
+                                                "How much knockback force to apply. Default is 15. Feel free to break physics.");
+
+            // Do the patching
+            harmony.PatchAll(typeof(LethalKnockbackMod));
+            harmony.PatchAll(typeof(ShotgunItemPatch));
+        }
+    }
+}
